7e88fc0ec338cd78fcbb8707303be2a31d3ab654
Angelo Scotto <a.scotto@reply.it>
2015-01-24 15:48:58 +0100

    Added portable version

---
+++ fuelscm.portable/tools/chocolateyInstall.ps1
@@ -0,0 +1,11 @@
+﻿try {
+  $package = 'fuelscm'
+  $PackageVersion = "0.9.6"
+  $url = "http://fuelscm.org/files/releases/Fuel-0.9.6-win32.zip"
+  $destination = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)" 
+
+  Install-ChocolateyZipPackage $package -url $url -unzipLocation $destination
+} catch {
+  Write-ChocolateyFailure $package "$($_.Exception.Message)"
+  throw
+}
