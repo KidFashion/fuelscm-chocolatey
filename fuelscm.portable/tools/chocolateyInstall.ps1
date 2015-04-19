bb277bd436c77e610091536ca08a6202c27359b5
Angelo Scotto <a.scotto@reply.it>
2015-04-19 11:43:14 +0200

    Updated to version 1.0.0 of fuel-scm

--- fuelscm.portable/tools/chocolateyInstall.ps1
+++ fuelscm.portable/tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿try {
   $package = 'fuelscm'
-  $PackageVersion = "0.9.6"
-  $url = "http://fuelscm.org/files/releases/Fuel-0.9.6-win32.zip"
+  $PackageVersion = "1.0.0"
+  $url = "http://fuelscm.org/files/releases/Fuel-1.0.0-win32.zip"
   $destination = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)" 
 
   Install-ChocolateyZipPackage $package -url $url -unzipLocation $destination
